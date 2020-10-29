71819173b5969ea209b3e95f5e22cfc35eb12a55
pedrootavio <pedrootavio.neto@gmail.com>
2020-10-28 22:36:03 -0300

    [Microservices com Spring Cloud - Aula 4] - Load Balancer e Spring Feign - Client Side Load Balancer

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -3,6 +3,7 @@ package br.com.alura.microservice.loja.service;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,17 @@ public class CompraService {
     @Autowired
     private RestTemplate client;
 
+    @Autowired
+    private DiscoveryClient eurekaClient;
+
     public void realizaCompra(CompraDTO compra) {
         ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://fornecedor/info/"
                         + compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+        eurekaClient.getInstances("fornecedor").stream()
+                .forEach(fornecedor -> {
+                    System.out.println("localhost:"+fornecedor.getPort());
+                });
 
         System.out.println(exchange.getBody().getEndereco());
     }
